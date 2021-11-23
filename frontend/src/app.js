4309400a581fb7b5fb5bc7ef30c7d1a46bf12801
Jere Luhtanen <Luhtanen.jere@gmail.com>
2021-11-23 15:08:38 +0200

    Added frontend structure

---
+++ frontend/src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/header';
+import Navigator from './components/navigator';
+import Body from './components/body';
+import './index.css';
+
+class App extends React.Component {
+
+    render(){
+     return(
+        <div classname="header">
+            <Header/>
+            <Navigator/>
+            <Body/>
+        </div>
+     )
+    }
+    
+}
+
+export default App;
--- simulator/simulator.js
+++ simulator/simulator.js
@@ -2,18 +2,16 @@
 
 const mqtt = require('mqtt');
 
-
 const addr = 'mqtt://127.0.0.1:1883';  //Brokers address
 const t = 2;  // modify to change the sending interval
+const client = mqtt.connect(addr);
+
 
 let pid = 1;
 let gid = 21;
 let timeNow = new Date().toString();
 let lat = 0.01;
 let lon = 0.02;
-
-const client = mqtt.connect(addr);
-
 
 setInterval(() => {
 	let obj = {
@@ -25,8 +23,8 @@ setInterval(() => {
 			longitude: lon
 		}
 	}
+
 	timeNow = Date();
-	console.log(timeNow);
-	console.log('published.');
 	client.publish('sim/testdata', JSON.stringify(obj)); // See that topic is correct
+
 }, t * 1000);
