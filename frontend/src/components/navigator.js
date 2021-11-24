f9951683c5fa619e346e23416382d8f807a0ead7
Jere Luhtanen <Luhtanen.jere@gmail.com>
2021-11-24 15:35:30 +0200

    Installed bootstrap framework for better navbar

--- frontend/src/components/navigator.js
+++ frontend/src/components/navigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavigatorItem from './navigatoritem';
+import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
 import '../index.css';
 
 
@@ -7,12 +7,27 @@ class Navigator extends React.Component {
 
     render(){
         return(
-            <div className="navcont">
-                <NavigatorItem name="Home" />
-                <NavigatorItem name="Map" />
-                <NavigatorItem name="Groups" />
-                <NavigatorItem name="About" />
-            </div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#linkgoeshere">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Groups</Nav.Link>
+                        <Nav.Link href="#">Tracking</Nav.Link>
+                        <Nav.Link href="#">About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    <NavDropdown title="More" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Sign in/Log in</NavDropdown.Item>
+                    </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+                </Navbar>
         )
     }
 }
